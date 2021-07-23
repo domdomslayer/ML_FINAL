34f98ac1905f2ad8e9d68c1f1a88ac0c2b4d6cec
Hoshi Iju <e195713@ie.u-ryukyu.ac.jp>
2021-07-23 22:39:53 +0900

    分類ラベルの数値化、ジャンルとしてOtherを選択しないよう変更

--- ML_final.py
+++ ML_final.py
@@ -5,9 +5,11 @@ import numpy as np
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 import requests
 
 def bow_tfidf(docs):
+    #TF-IDFで重みづけされたBag-of-Words形式の特徴ベクトルの生成
     vectorizer = fe_text.TfidfVectorizer(norm=None, stop_words='english')
     vectors = vectorizer.fit_transform(docs)
     return vectors.toarray(), vectorizer  
@@ -22,14 +24,21 @@ def pre_processing(text, mode):
 
 def get_genre(document):
     #HTMLファイルからジャンル情報を取得
+    genre = ""
     word_list=document.split()
     for index, word in enumerate(word_list):
         if word == "Genre:":
-            if word_list[index + 1][-1] == ",":
-                    word_list[index + 1]= word_list[index + 1].strip(",")
-            return word_list[index + 1]
+            #第１ジャンルが「その他」の場合は第２ジャンルを取得
+            if word_list[index + 1] == "Other,":
+                genre = word_list[index + 2]
+            else:
+                genre = word_list[index + 1]
+            if genre[-1] == ",":
+                    genre = genre.strip(",")
+            return genre
 
 def cut_common_part(document):
+    #全ページ共通な箇所をテキストから削除する
     text = ""
     isUnnecessary = True
     word_list = document.split()
@@ -74,13 +83,22 @@ for document in documents_list:
 #Bag-of-words形式の特徴ベクトルを生成
 vectors_tfidf, vectorizer_tfidf = bow_tfidf(docs)
 
+#String型の分類ラベルを数値に変換
+le = LabelEncoder()
+genre_id = le.fit_transform(genre_list)
+print(le.classes_) #ラベルと数値の対応確認
+
+np.insert(vectors_tfidf, vectors_tfidf.shape[1], genre_id, axis=1)
+print(vectors_tfidf[1])
+
+#データセットを訓練データとテストデータに分割
 X_train, X_test, y_train, y_test = train_test_split(vectors_tfidf, genre_list)
 
 #分類モデルには線形SVCを用いる
-#clf=svm.LinearSVC()
-clf=RandomForestClassifier()
+clf=svm.LinearSVC()
+#clf=RandomForestClassifier()
 clf.fit(X_train, y_train)
-print(clf.score(X_test, y_test))
+#print(clf.score(X_test, y_test))
 
 for fh in fh_list:
     fh.close()
